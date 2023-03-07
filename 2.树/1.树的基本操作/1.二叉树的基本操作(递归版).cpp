f6ae025a4240b4c07dbc0c8e90927b93d9441b15
Hope-bend <94051000+Hope-bend@users.noreply.github.com>
2023-03-07 15:57:57 +0800

    1.二叉树的基本操作递归版

---
+++ 2.树/1.树的基本操作/1.二叉树的基本操作(递归版).cpp
@@ -0,0 +1,146 @@
+#include<iostream>
+using namespace std;
+
+typedef struct Node
+{
+	char data;
+	struct Node *lchild, *rchild;
+}BiTNode, *BiTree;
+
+//先序创建二叉树
+void CreateBiTree(BiTree &T)
+{ 
+	char ch;
+	cin >> ch;
+	if(ch == '#')T = NULL;
+	else
+	{
+		T = new BiTNode;
+		T->data = ch;
+		CreateBiTree(T->lchild);
+		CreateBiTree(T->rchild);
+	}
+}
+
+//先序遍历
+void PreOrder(BiTree T)
+{
+	if(T)
+	{
+		cout << T->data;
+		PreOrder(T->lchild);
+		PreOrder(T->rchild);
+	}
+} 
+
+
+
+//中序遍历
+void InOrder(BiTree T)
+{
+	if(T)
+	{
+		InOrder(T->lchild);
+		cout << T->data;
+		InOrder(T->rchild);
+	};
+} 
+
+//树的深度
+int Depth(BiTree T)
+{
+	if(T == NULL)return 0;
+	else
+	{
+		int i = Depth(T->lchild);
+		int j = Depth(T->rchild);
+		return i > j ? (i + 1) : (j + 1);
+	}
+}
+
+int main()
+{
+	BiTree T;
+	cout << "先序创建二叉树:";
+	//abc##de#g##f###创建二叉树的用例 
+	CreateBiTree(T);
+	cout << endl;
+	cout << "先序遍历二叉树:";
+	PreOrder(T);
+	cout << endl;
+	cout << "中序遍历二叉树:";
+	InOrder(T);
+	cout << endl;
+	cout << "树的深度:" << Depth(T) << endl;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+ 
